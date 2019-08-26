fa7e18569b02b8fcdaa7951846a1786b779612f1
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-26 18:51:02 -0500

    example http module

---
+++ server.js
@@ -0,0 +1,14 @@
+import config, { nodeEnv } from "./config";
+import http from "http";
+
+const server = http.createServer();
+
+server.listen(8080);
+
+server.on("request", (req, res) => {
+  res.write("Hello HTTP!\n");
+  setTimeout(() => {
+    res.write("I can stream \n");
+    res.end();
+  }, 3000);
+});
