27f3d5843ec0507ea2bc148a61c8b7425b407a0f
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-26 19:19:10 -0500

    added ejs

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const server = express();
 server.set("view engine", "ejs");
 
 server.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    content: "lorem ipsum dolor amet"
+  });
 });
 
 server.use(express.static("public"));
